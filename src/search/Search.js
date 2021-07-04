b7d2305f1abdd2ef79988563d2fbec9d2d1c416d
meleciosilva <mpsilva94@gmail.com>
2021-07-04 11:36:24 -0400

    move all logic to Layout and implement useContext

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,24 +1,21 @@
-import { useState } from "react";
-import ErrorAlert from "../layout/ErrorAlert";
+import { useContext } from "react";
+import ErrorAlert from "../shared/ErrorAlert";
 import SearchBar from "./SearchBar";
 import ListArticles from "./ListArticles";
-import useRequest from "../hooks/useRequest";
+
+import { LayoutContext } from "../contexts/LayoutContext";
 
 function Search() {
-
-  const [search, setSearch] = useState(null);
-  const { data: { hits }, loading, error } = useRequest(`http://hn.algolia.com/api/v1/search?query=${search}`);
-
-  function handleSubmit(event, input) {
-    event.preventDefault();
-    setSearch(input);
-    console.log(input);
-  }
   
+  const { 
+    data: { data: { hits }, loading, error },
+    search
+  } = useContext(LayoutContext);
+
   if (loading) return <h1>Loading...</h1>
   return (
     <div className="px-2 py-3">
-      <SearchBar handleSubmit={handleSubmit} />
+      <SearchBar />
       <ErrorAlert error={error} />
       { !search ? <h1>No Articles Found</h1> : <ListArticles articles={hits} /> }
     </div>
