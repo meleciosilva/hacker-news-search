73e1703dcfa5cdcb5d94163cf053bb5b71543190
meleciosilva <mpsilva94@gmail.com>
2021-07-04 13:38:18 -0400

    update useEffect to use async/await

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,25 +1,35 @@
 import { useState, useEffect } from "react";
-const axios = require("axios");
 
 const useRequest = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(fetchData, [url]);
-
-  function fetchData() {
+  useEffect(() => {
+    setLoading(true);
     setData([]);
     setError(null);
     const abortController = new AbortController();
-      setLoading(true);
-      axios.get(url, { signal: abortController.signal })
-        .then((response) => setData(response.data))
-        .then(() => setLoading(false))
-        .catch(setError)
-    return () => abortController.abort();
-  }
+    async function fetchData() {
+      try {
+        const response = await fetch(url, { signal: abortController.signal });
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+      } catch(err) {
+          if (err.name === "AbortError") {
+            return console.log("Request Aborted");
+          } else {
+            setError(err);
+          }
+        }
+      }
+    fetchData();
 
+    return () => abortController.abort();
+    
+  }, [url]);
+  
   return { data, loading, error };
   
 }
