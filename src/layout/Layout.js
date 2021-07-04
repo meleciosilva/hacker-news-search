7127b032f024556f8adb2a85d1a9e4738bfec982
meleciosilva <mpsilva94@gmail.com>
2021-07-04 07:07:52 -0400

    return Menu and Routes components

---
+++ src/layout/Layout.js
@@ -0,0 +1,13 @@
+import Menu from "./Menu";
+import Routes from "./Routes";
+
+function Layout() {
+  return (
+    <div>
+      <Menu />
+      <Routes />
+    </div>
+  )
+}
+
+export default Layout;
