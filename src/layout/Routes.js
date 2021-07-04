49ba3f4c466766a7289573fd7820257f8dc5101e
meleciosilva <mpsilva94@gmail.com>
2021-07-04 11:34:01 -0400

    update "/" route to return Home heading

--- src/layout/Routes.js
+++ src/layout/Routes.js
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Search from "../search/Search";
 import History from "../history/History";
 import NotFound from "./NotFound";
@@ -7,7 +7,7 @@ function Routes() {
   return (
     <Switch>
       <Route exact={true} path="/">
-        <Redirect to={"/"}/>
+        <h1 className="p-2">Home</h1>
       </Route>
       <Route path="/search">
         <Search />
