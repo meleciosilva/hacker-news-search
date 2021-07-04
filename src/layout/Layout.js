2c325f6149875a3f3dfc75b17aa331fc3b9fb050
meleciosilva <mpsilva94@gmail.com>
2021-07-04 11:33:11 -0400

    implement LayoutContext and define its values

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
 import Menu from "./Menu";
 import Routes from "./Routes";
+import { LayoutContext } from "../contexts/LayoutContext";
+import useRequest from "../hooks/useRequest";
 
 function Layout() {
+
+  const [search, setSearch] = useState(null);
+  const [history, setHistory] = useState([]);
+  
+  const data = useRequest(`http://hn.algolia.com/api/v1/search?query=${search}`);
+  
+  const handleSubmit = (event, input) => {
+    event.preventDefault();
+    setSearch(input);
+    setHistory(prevState => [...prevState, input]);
+  }
+
   return (
-    <div>
+    <LayoutContext.Provider value={{ data, handleSubmit, search, history }}>
       <Menu />
       <Routes />
-    </div>
+    </LayoutContext.Provider>
   )
 }
 
