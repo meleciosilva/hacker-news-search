007f8047aabf8de572214627a8851fa17ee93825
meleciosilva <mpsilva94@gmail.com>
2021-07-04 09:20:41 -0400

    use useRequest hook and update conditional render

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,45 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ErrorAlert from "../layout/ErrorAlert";
 import SearchBar from "./SearchBar";
 import ListArticles from "./ListArticles";
-const axios = require("axios");
+import useRequest from "../hooks/useRequest";
 
 function Search() {
-  
+
   const [search, setSearch] = useState(null);
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(fetchArticles, [search]);
-
-  function fetchArticles() {
-    setArticles([]);
-    setError(null);
-    const abortController = new AbortController();
-    if (search) {
-      setLoading(true);
-      const url = `http://hn.algolia.com/api/v1/search?query=${search}`;
-      axios.get(url, { signal: abortController.signal })
-        .then((response) => setArticles(response.data.hits))
-        .then(() => setLoading(false))
-        .catch(setError)
-    }
-    return () => abortController.abort();
-  }
+  const { data: { hits }, loading, error } = useRequest(`http://hn.algolia.com/api/v1/search?query=${search}`);
 
   function handleSubmit(event, input) {
     event.preventDefault();
     setSearch(input);
-    console.log(input)
-
+    console.log(input);
   }
   
+  if (loading) return <h1>Loading...</h1>
   return (
     <div className="px-2 py-3">
-      <SearchBar handleSubmit={handleSubmit}/>
+      <SearchBar handleSubmit={handleSubmit} />
       <ErrorAlert error={error} />
-      { loading ? <h1>Loading...</h1> : <ListArticles articles={articles} /> }
+      { !search ? <h1>No Articles Found</h1> : <ListArticles articles={hits} /> }
     </div>
   )
 }
